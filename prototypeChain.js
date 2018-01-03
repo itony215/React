b5d1817129fd1d5901e40d212e92ae05c0e0cb44
tony <itony215@gmail.com>
2018-01-03 17:01:24 +0800

    callback

--- prototypeChain.js
+++ prototypeChain.js
@@ -10,3 +10,7 @@ let car = new Car("green");
 //取得並在將原型物件顯示出來
 let carProto = Object.getPrototypeOf(car); //Car 原型物件
 console.log(carProto);
+let objProto = Object.getPrototypeOf(carProto); // 原型物件
+console.log(objProto);
+let lastOne = Object.getPrototypeOf(objProto); //原型鍊的終點：null
+console.log(lastOne);
